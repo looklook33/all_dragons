640b02c8a0b4966afb2bab0de51c01b8ef285e12
Minyu Zhang <minyuzhang@hotmail.com>
2024-06-04 16:42:27 -0500

    finished dragon

--- all-dragons.js
+++ all-dragons.js
@@ -1,10 +1,23 @@
 // Your code here 
+const FriendlyDragon = require('./classes/friendly-dragon');
+const EvilDragon = require('./classes/evil-dragon');
+const Dragon = require('./classes/dragon');
 
+falkor = new FriendlyDragon('Falkor', 'white', ["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], "Bastian")
 
+smaug = new EvilDragon('Smaug', 'black', ["take over your mountain kingdom", "steal all your dwarven gold", "burn down your floating village"], 'Dwarf King')
+
+allDragons = Dragon.getDragons(falkor, smaug)
+
+console.log(falkor);
+falkor.hasLifeGoals();
+console.log(falkor.breathesFire());
+console.log(falkor.helpsPeople());
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
+  module.exports.allDragons = allDragons;
   module.exports.falkor = falkor
   module.exports.smaug = smaug
 } catch {
--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,22 @@
 // Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...dragon) {
+    return dragon.map(ele => ele.name)
+
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
